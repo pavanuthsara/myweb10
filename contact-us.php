57d4fd70872ec123d28de713292245970f5d02b7
Pavan Uthsara <uthsarapavan@gmail.com>
2024-10-05 20:32:21 +0530

    v5.0

--- contact-us.php
+++ contact-us.php
@@ -5,7 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Contact Us - Shield Plus</title>
 	<link rel="stylesheet" type="text/css" href="./css/contact-us.css">
-    <script src="javascript/contact-us.js"></script>
+    
+    
 </head>
 
 <body>
@@ -15,28 +16,20 @@
 	
     <div class="contact-details">
 	<div class = "c1">
-            <h1>Contact Shield Plus</h2>
+            <h1>Contact Apartment INC</h2>
             <p>If you have any questions or need assistance, we're here to help!</p>
-            <button onclick="askContactNow()">Contact Us Now</button>
+            <!-- <button onclick="askContactNow()">Contact Us Now</button> -->
 
-            <p>Do you require help with your health insurance? Speak with Shield Plus right now for individualised assistance. 
-            Our staff is available to assist you in locating the ideal insurance plan to meet your needs. Contact us by phone, email, or 
+            <p>Do you require help with your health insurance? Speak with Shield Plus right now for individualised assistance. Our staff is available to assist you in locating the ideal insurance plan to meet your needs. Contact us by phone, email, or 
             by completing the form below. Our top priorities are your well-being and mental clarity!</p>
         </div>
 
         <div class = "forms">
-        <fieldset>
-        <legend>Fill the form</legend>
-        <form action="contact.php" method="post">
-            <label>Your Name:</label><br>
-            <input type="text" id="name" name="name" required><br><br>
-            <label>Your Email:</label><br>
-            <input type="email" id="email" name="email" required><br><br>
-            <label>Message:</label><br>
-            <textarea id="message" name="message" required></textarea><br><br>
-            <input type="button" id="submitButton" value="Submit" onclick="myfunction()">
-        </form>
-        </fieldset>
+        <button id="contactBtn" style="background-color: #6A9AB0; color: black; padding: 10px; font-size: 20px; border: none; border-radius: 10px; cursor: pointer;">
+            Contact Us
+        </button>
+
+        <p id="demo"></p>
 
         <br><br>
 
@@ -44,20 +37,22 @@
             <legend><b>Contact us via</b></legend>  
             <div class = "det">
                 Adress : 124, Colombo 7, Galle Face<br><br>
-                Phone : +94 11 5 745 856 <br><br>
-                Email : shieldplusinsurance@gmail.com<br><br>
-                <a href="#">web : www.shieldplus.com</a>
+                Email : ApartmentINC@gmail.com<br><br>
+                <a href="#">web : www.ApartmentINC.com</a>
             </div>
         </fieldset>	
 
 	<footer>
 		<hr>
-		&copy; 2024 Copyright Reserved - Shield Plus Insurance <br>
+		&copy; 2024 Copyright Reserved - Apartment INC <br>
 		<small>email@shieldplus.com</small>
 	</footer>
     </div>
 	</div>
 
 
+
 </body>
+
+<script src="javascript/contactus.js"></script>
 </html>
---
+++ javascript/contactus.js
@@ -0,0 +1,8 @@
+
+
+const element = document.getElementById("contactBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "<a href='#'>Whatsapp - 077 126 7856</a>";
+}
